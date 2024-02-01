ea85511ad34635c23aa2533a612a2c157ace5cd7
Jason Montleon <jmontleo@redhat.com>
2024-02-01 11:21:16 -0500

    as some band once said. whats going on?

--- annotate.sh
+++ annotate.sh
@@ -2,10 +2,13 @@
 
 # shellcheck disable=SC2086
 
-set -euo pipefail
+set -xeuo pipefail
+
+"${TRUNK_PATH}" check github_annotate --help
 
 "${TRUNK_PATH}" check github_annotate \
   --ci \
+  --verbose \
   --upstream HEAD \
   --github-commit "${GITHUB_EVENT_WORKFLOW_RUN_HEAD_SHA}" \
   --github-label "${INPUT_LABEL}" \
